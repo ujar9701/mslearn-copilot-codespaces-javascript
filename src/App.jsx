9b350d1631a98704900bd05c56dc7b2f4f0a3e69
Raju Thatha <rajrayudu01@gmail.com>
2025-08-19 05:25:03 +0000

    Update siteProps with user information and enhance social icon animations

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Raju Thatha",
+  title: "Automation Quality Assurance Associate",
+  email: "rajrayudu01@gmail.com",
+  gitHub: "ujaR9701",
+  instagram: "rayudu4",
+  linkedIn: "rajrayudu",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
